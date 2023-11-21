c30b158e977f14d471eba828e8745f80e394c340
Lucas Snel <7912315+elbojoloco@users.noreply.github.com>
2023-11-21 15:28:30 +0100

    Implement Lift with a nullable and fillable field

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,43 +3,41 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use WendellAdriel\Lift\Attributes\Cast;
+use WendellAdriel\Lift\Attributes\Fillable;
+use WendellAdriel\Lift\Attributes\Hidden;
+use WendellAdriel\Lift\Attributes\PrimaryKey;
+use WendellAdriel\Lift\Lift;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, Lift;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    #[PrimaryKey]
+    public int $id;
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    #[Fillable]
+    public string $name;
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
-    ];
+    #[Fillable]
+    public ?string $display_name;
+
+    #[Fillable]
+    public string $email;
+
+    #[Fillable]
+    #[Hidden]
+    #[Cast('hashed')]
+    public string $password;
+
+    #[Hidden]
+    public string $remember_token;
+
+    #[Cast('datetime')]
+    public ?Carbon $email_verified_at;
 }
